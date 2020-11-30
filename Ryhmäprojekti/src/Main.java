4e32b20c068a17997e9500618a3fa4adea20c1c0
A-d-f <anna-maria.palm@student.hamk.fi>
2020-11-30 17:58:15 +0200

    Mitä ihmettä...

--- Ryhmäprojekti/src/Main.java
+++ Ryhmäprojekti/src/Main.java
@@ -3,29 +3,25 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Scanner in=new Scanner(System.in);
+
+		// for (int i = 0 ; i < 1000; i++) { //looppi tietojen vienti‰ varten
+		// tietovienti.vietieto(); }
+
+		Scanner in = new Scanner(System.in);
 		String vastaus;
-		TulostaOtsikko();
-		
-		
-		System.out.println(" ");
-		System.out.println("Haluatko tilata s‰mpyl‰n?(k/e)?");
-		vastaus=in.nextLine();
-		
-	}
-	
-	public static void TulostaTaytteet() {
-		//tietokannassa olevien s‰mpyl‰/t‰yte/kastikevaihtoehtojen tulostus t‰h‰n
-	}
-	
-	public static void TulostaOtsikko() {
-		System.out.println("__________         __                 __   .__.__               .__          \r\n" + 
-				"\\______   \\_____ _/  |_  ____   ____ |  | _|__|  |  __ __  ____ |  | _____   \r\n" + 
-				" |     ___/\\__  \\\\   __\\/  _ \\ /    \\|  |/ /  |  | |  |  \\/  _ \\|  | \\__  \\  \r\n" + 
-				" |    |     / __ \\|  | (  <_> )   |  \\    <|  |  |_|  |  (  <_> )  |__/ __ \\_\r\n" + 
-				" |____|    (____  /__|  \\____/|___|  /__|_ \\__|____/____/ \\____/|____(____  /\r\n" + 
-				"                \\/                 \\/     \\/                              \\/");
+		tulostaOtsikko();
+		tietotuonti.tiedot();
+		System.out.println("\n" + "Haluatko tilata s‰mpyl‰n?(k/e)?");
+		vastaus = in.nextLine();
+
 	}
 
-} //Main p‰‰ttyy
+	public static void tulostaOtsikko() {
+		System.out.println("__________         __                 __   .__.__               .__          \r\n"
+				+ "\\______   \\_____ _/  |_  ____   ____ |  | _|__|  |  __ __  ____ |  | _____   \r\n"
+				+ " |     ___/\\__  \\\\   __\\/  _ \\ /    \\|  |/ /  |  | |  |  \\/  _ \\|  | \\__  \\  \r\n"
+				+ " |    |     / __ \\|  | (  <_> )   |  \\    <|  |  |_|  |  (  <_> )  |__/ __ \\_\r\n"
+				+ " |____|    (____  /__|  \\____/|___|  /__|_ \\__|____/____/ \\____/|____(____  /\r\n"
+				+ "                \\/                 \\/     \\/                              \\/");
+	}
+} // Main p‰‰ttyy
