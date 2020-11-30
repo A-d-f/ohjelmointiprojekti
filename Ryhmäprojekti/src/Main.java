d93c2d3b2b03ac1b8b8d99913c12d2de0a0eb285
A-d-f <anna-maria.palm@student.hamk.fi>
2020-11-30 13:40:55 +0200

    TulostaOtsikko lisätty

--- Ryhmäprojekti/src/Main.java
+++ Ryhmäprojekti/src/Main.java
@@ -6,13 +6,8 @@ public class Main {
 		
 		Scanner in=new Scanner(System.in);
 		String vastaus;
+		TulostaOtsikko();
 		
-		System.out.println("__________         __                 __   .__.__               .__          \r\n" + 
-				"\\______   \\_____ _/  |_  ____   ____ |  | _|__|  |  __ __  ____ |  | _____   \r\n" + 
-				" |     ___/\\__  \\\\   __\\/  _ \\ /    \\|  |/ /  |  | |  |  \\/  _ \\|  | \\__  \\  \r\n" + 
-				" |    |     / __ \\|  | (  <_> )   |  \\    <|  |  |_|  |  (  <_> )  |__/ __ \\_\r\n" + 
-				" |____|    (____  /__|  \\____/|___|  /__|_ \\__|____/____/ \\____/|____(____  /\r\n" + 
-				"                \\/                 \\/     \\/                              \\/");
 		
 		System.out.println(" ");
 		System.out.println("Haluatko tilata s‰mpyl‰n?(k/e)?");
@@ -22,6 +17,15 @@ public class Main {
 	
 	public static void TulostaTaytteet() {
 		//tietokannassa olevien s‰mpyl‰/t‰yte/kastikevaihtoehtojen tulostus t‰h‰n
+	}
+	
+	public static void TulostaOtsikko() {
+		System.out.println("__________         __                 __   .__.__               .__          \r\n" + 
+				"\\______   \\_____ _/  |_  ____   ____ |  | _|__|  |  __ __  ____ |  | _____   \r\n" + 
+				" |     ___/\\__  \\\\   __\\/  _ \\ /    \\|  |/ /  |  | |  |  \\/  _ \\|  | \\__  \\  \r\n" + 
+				" |    |     / __ \\|  | (  <_> )   |  \\    <|  |  |_|  |  (  <_> )  |__/ __ \\_\r\n" + 
+				" |____|    (____  /__|  \\____/|___|  /__|_ \\__|____/____/ \\____/|____(____  /\r\n" + 
+				"                \\/                 \\/     \\/                              \\/");
 	}
 
 } //Main p‰‰ttyy
