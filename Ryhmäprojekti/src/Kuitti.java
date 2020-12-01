f8d4bfa86a4977f3afe3c62eb468f40c34b03934
A-d-f <anna-maria.palm@student.hamk.fi>
2020-12-01 14:24:26 +0200

    pvm kuittiin

--- Ryhmäprojekti/src/Kuitti.java
+++ Ryhmäprojekti/src/Kuitti.java
@@ -8,11 +8,12 @@ public class Kuitti {
 	//tämä mainiin? tulostaTiedostoon("Ostot:\n " +  + " . Summa: " + veloitus + "€.");
 	
 	
-	public void tulostaTiedostoon(String txt) {
+	public void tulostaTiedostoon(String txt, String kuitti) {
 
 		try {
-
+			java.util.Date date = new java.util.Date();
 			FileWriter fwriter = new FileWriter(kuitti, true);
+			fwriter.write(date.toString());
 			fwriter.write(txt + "\n");
 			fwriter.close();
 		} catch (Exception e) {
